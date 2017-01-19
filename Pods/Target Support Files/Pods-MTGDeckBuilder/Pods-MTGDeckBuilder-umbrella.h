79ed1dc696109b08f7a85e251bb86c7282f36061
sentiinex <sentiinex@gmail.com>
2017-01-18 17:13:52 -0800

    fixed(?) renaming issue

---
+++ Pods/Target Support Files/Pods-MTGDeckBuilder/Pods-MTGDeckBuilder-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_MTGDeckBuilderVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_MTGDeckBuilderVersionString[];
+
